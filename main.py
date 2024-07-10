c27706dee15a71df3b636c538c18259e0640c3ec
monukoni <kartoplya213@gmail.com>
2024-07-10 14:15:41 +0300

    update

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
-import pyautogui
-import random
-import tkinter as tk
+from PyQt5 import QtCore, QtGui
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QPixmap, QImage
+import sys
 
-x = 1800
-y = 894
-x_size = 128
-y_size = 128
+mascot_size = [128, 128]
+taskbar_size = 57
+
+class Window(QMainWindow):
+    def __init__(self, x=0, y=0):
+        super().__init__()
+
+        self.setGeometry(x, y, *mascot_size)
+
+        # Налаштування вікна
+        self.setWindowTitle('Steppy')
+        self.setWindowIcon(QtGui.QIcon('cat256.png'))
+        self.setWindowFlag(QtCore.Qt.WindowType.FramelessWindowHint)
+        self.setWindowFlag(QtCore.Qt.WindowType.WindowStaysOnTopHint)
+        self.setAttribute(QtCore.Qt.WidgetAttribute(0x78)) # Не шарю шо це
+        self.setAutoFillBackground(True)
+
+        # Ставимо зображення
+        self.img = QLabel(self)
+        transparent_img = QImage()
+        transparent_img.load(f'cat{mascot_size[0]}.png')
+        self.img.setPixmap(QPixmap.fromImage(transparent_img))
+        self.img.resize(self.width(), self.height())
+
+        self.show()
 
 
-
-
-def update():
-    frame = image
-    window.geometry(f'{x_size}x{y_size}+{x}+{y}')
-    label.configure(image=frame)
-
-
-
-window = tk.Tk()
-
-image = tk.PhotoImage(file='cat128.png', format='png')
-
-# window configuration
-window.config(highlightbackground='grey')
-label = tk.Label(window, bd=0, bg='grey')
-window.overrideredirect(True)
-window.wm_attributes('-topmost', 1)
-window.wm_attributes('-transparentcolor', 'grey')
-label.pack()
-window.after(1, update)
-window.mainloop()
+if __name__ == '__main__':
+    App = QApplication(sys.argv)
+    window = Window(1920-mascot_size[0], 1080-mascot_size[1]-taskbar_size)
+    sys.exit(App.exec())
