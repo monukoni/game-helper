ae4d643795321c1d3f18cc9e32a58a37788eb7bc
monukoni <kartoplya213@gmail.com>
2024-07-15 17:39:01 +0300

    cleaning

--- main.py
+++ main.py
@@ -9,7 +9,6 @@ class Window(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.new_window = None
         self.mascot_size = [128, 128]
         self.taskbar_size = (QDesktopWidget().screenGeometry().height()
                              - QDesktopWidget().availableGeometry().height())
@@ -28,7 +27,7 @@ class Window(QMainWindow):
         self.setWindowIcon(QtGui.QIcon('cat256.png'))
         self.setWindowFlag(QtCore.Qt.WindowType.FramelessWindowHint)
         self.setWindowFlag(QtCore.Qt.WindowType.WindowStaysOnTopHint)
-        self.setAttribute(QtCore.Qt.WidgetAttribute(0x78))  # Не шарю шо це
+        self.setAttribute(QtCore.Qt.WidgetAttribute(0x78))
         self.setAutoFillBackground(True)
 
         # Ставимо зображення
@@ -40,62 +39,18 @@ class Window(QMainWindow):
 
         self.show()
 
-    def moveEvent(self, event):
-        if self.new_window is not None:
-            # TODO bug
-            self.new_window.move(self.x()-int((self.new_window.width() - 128/1.5)), self.y()-120)
-
     def mousePressEvent(self, event: QtGui.QMouseEvent):
-
+        self.current_position = event.globalPos()
 
         if event.button() == QtCore.Qt.RightButton:
-            if self.new_window is not None:
-                if self.new_window.isVisible() is False:
-                    self.new_window = None
-
-            if self.new_window is None:
-                self.popup_menu.set_pos(QPoint(event.globalPos().x(),self.pos().y()))
-                self.popup_menu.show()
-                # self.new_window = SpeechBubble(self.x(), self.y())
-                # self.new_window.show()
-        self.current_position = event.globalPos()
+            self.popup_menu.show()
+            self.popup_menu.set_pos(QPoint(event.globalPos().x(),
+                                           self.pos().y() - self.popup_menu.height()))
 
     def mouseMoveEvent(self, event: QtGui.QMouseEvent):
         delta = QPoint(event.globalPos() - self.current_position)
         self.move(self.x() + delta.x(), self.y() + delta.y())
         self.current_position = event.globalPos()
-
-    def closeEvent(self, event: QtGui.QCloseEvent):
-        pass
-
-
-class SpeechBubble(QtWidgets.QMenu):
-    def __init__(self, x, y):
-        super().__init__()
-        self.setWindowFlag(QtCore.Qt.WindowType.FramelessWindowHint)
-        self.setWindowFlag(QtCore.Qt.WindowType.WindowStaysOnTopHint)
-
-        central_widget = QWidget(self)
-        self.setCentralWidget(central_widget)
-        layout = QVBoxLayout()
-        central_widget.setLayout(layout)
-
-        self.label = QLabel(self)
-        self.label.setText("А ви знали, що в туалеті можна какати?")
-        self.label.setWordWrap(True)
-        self.label.setAlignment(QtCore.Qt.AlignCenter)
-        layout.addWidget(self.label)
-
-        self.close_button = QPushButton(self)
-        self.close_button.setText("×")
-        # self.close_button.setAlignment(QtCore.Qt.AlignCenter)
-        # self.close_button.setGeometry(180, 0, 20, 20)
-        self.close_button.clicked.connect(self.buttonClose)
-
-        self.move(x-int((self.width()-128)/10), y-120)
-
-    def buttonClose(self):
-        self.close()
 
 
 class PopupMenu(QtWidgets.QMenu):
@@ -103,20 +58,51 @@ class PopupMenu(QtWidgets.QMenu):
         super(PopupMenu, self).__init__(parent)
         self.layout = QtWidgets.QVBoxLayout()
         self.setWindowFlag(QtCore.Qt.WindowType.WindowStaysOnTopHint)
+
         widgets = [
-            # QTextEdit
-            # QInputDialog
+            PopupButtonLabel("search", self.showMenu, self), # пхаємо функцію на лейбл
+            PopupButtonLabel("create tips", None, self),
+            PopupButtonLabel("make notes", None, self),
+            ExitButton("Exit", self)
         ]
         for widget in widgets:
-            self.layout.addWidget(widget())
+            self.layout.addWidget(widget)
+        self.layout.addStretch()
         self.setLayout(self.layout)
-        # self.setGeometry(self.currentPosition.x(),
-        #                  self.currentPosition.y(),
-        #                  *self.mascotSize)
 
+    def showMenu(self):
+        aboba = QMenu(self)
+        aboba.setGeometry(500, 500, 500, 500)
+        aboba.show()
 
     def set_pos(self, point: QPoint):
-        self.setGeometry(point.x() - 100, point.y()-128, 100, 100)
+        self.setGeometry(point.x() - 50, point.y() - 10, 100, 100)
+
+
+class PopupButton(QPushButton):
+    def __init__(self, text: str, parent=None):
+        super(PopupButton, self).__init__(parent)
+        self.setText(text)
+
+
+class PopupButtonLabel(QLabel):
+    def __init__(self, text: str, func=None, parent=None):
+        super(QLabel, self).__init__(parent)
+        self.setText(text)
+        self.func = func
+
+    def mousePressEvent(self, event):
+        if self.func is not None:
+            self.func()
+
+
+class ExitButton(PopupButtonLabel):
+    def __init__(self, text: str, parent=None):
+        super(PopupButtonLabel, self).__init__(text, parent)
+
+    def mousePressEvent(self, event, q_mouse_event=None):
+        sys.exit()
+
 
 if __name__ == '__main__':
     App = QApplication(sys.argv)
